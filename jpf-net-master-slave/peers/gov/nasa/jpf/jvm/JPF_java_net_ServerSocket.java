5b4adae86c51798396045c3a0f8e298d416a70be
Sergio Feo <sergio.feo@gmail.com>
2013-03-29 09:30:18 +0000

    Started intercepting ServerSocket method calls.

--- jpf-net-master-slave/peers/gov/nasa/jpf/jvm/JPF_java_net_ServerSocket.java
+++ jpf-net-master-slave/peers/gov/nasa/jpf/jvm/JPF_java_net_ServerSocket.java
@@ -6,46 +6,31 @@ import gov.nasa.jpf.JPF;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import jp.ac.nii.masterslavemc.Channel;
+import jp.ac.nii.masterslavemc.NetworkLayer;
+import jp.ac.nii.masterslavemc.Channel.ChannelType;
+
 //import gov.nasa.jpf.network.SystemInfo;
 //import gov.nasa.jpf.network.cache.CacheLayer;
 
 public class JPF_java_net_ServerSocket {
-
+	public static final NetworkLayer net = NetworkLayer.getInstance();
 	private static Logger log = JPF.getLogger("gov.nasa.jpf.jvm");
 
-	private static boolean isInitialized = false;
-
-	public static int native_accept____I(MJIEnv env, int objRef) throws IOException {
-//		CacheLayer cache = CacheLayer.getInstance();
+	public static int native_accept(MJIEnv env, int objRef) throws IOException {
 		int port = env.getIntField(objRef, "port");
-		
-//		return cache.accept(port);
-		return 0;
+		int newSocket = -1;
+		if (net.accept(port)){
+		 newSocket = env.newObject("java.net.Socket");
+		}
+		return newSocket;
 	}
 
-	public static boolean isClosed(MJIEnv env, int objRef) {
-//		return CacheLayer.getInstance().isServerSocketClosed();
-		return false;
-	}
-
-	private static void init(MJIEnv env) {
-		Config cfg = env.getConfig();
-//		String boot_peer_cmd = cfg.getString(SystemInfo.BOOT_CMD);
-		String arg = "";
-
-//		log.info(String.format("[SERVER_SOCKET_PEER : accept()] %s=%s", SystemInfo.BOOT_CMD, boot_peer_cmd));
-
-//		CacheLayer.addBootPeerCommand(boot_peer_cmd);
-
-//		for (int i = 0; arg != null; i++) {
-//			arg = cfg.getString(SystemInfo.CMD_ARG + i);
-
-//			if (arg != null)
-//				CacheLayer.addBootPeerCommand(arg);
-
-//			log.info(String.format("[SERVER_SOCKET_PEER : accept()] %s%d=%s", SystemInfo.CMD_ARG, i, arg));
-//		}
-
-		isInitialized = true;
+	public static boolean native_init(MJIEnv env, int objRef, int port) {
+		Channel newC =  new Channel(ChannelType.SERVER, port);
+		if (!net.containsKey(newC))
+			net.newChannel(ChannelType.SERVER, port);
+		else return false;
+		return true;
 	}
 }
