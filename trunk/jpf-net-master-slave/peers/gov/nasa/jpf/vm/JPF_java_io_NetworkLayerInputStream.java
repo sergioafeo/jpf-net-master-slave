1387c2750c904454f55028c3e0a0d0821338dd21
sergio_feo <sergio_feo@fc204895-3c1d-4461-b7b7-69cea308e62d>
2013-04-18 08:54:40 +0000

    Fixed v7 peers

--- trunk/jpf-net-master-slave/peers/gov/nasa/jpf/vm/JPF_java_io_NetworkLayerInputStream.java
+++ trunk/jpf-net-master-slave/peers/gov/nasa/jpf/vm/JPF_java_io_NetworkLayerInputStream.java
@@ -1,12 +1,16 @@
 package gov.nasa.jpf.vm;
 
+import gov.nasa.jpf.annotation.MJI;
+
 import java.util.Set;
 
 import jp.ac.nii.masterslavemc.NetworkLayer;
 import jp.ac.nii.masterslavemc.NetworkMessage;
 
-public class JPF_java_io_NetworkLayerInputStream {
+public class JPF_java_io_NetworkLayerInputStream extends NativePeer {
 	private static NetworkLayer net = NetworkLayer.getInstance();
+	
+	@MJI
 	public static int native_read__I___3I(MJIEnv env, int robj, int sockID){
 		Set<NetworkMessage> bytes = net.read(env, sockID);
 		if (bytes != null && bytes.size() > 0){
